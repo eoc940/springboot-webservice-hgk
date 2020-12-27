ddf6e4daaf03806aba89c616535a56efc854367e
eoc940 <hgk0000@naver.com>
2020-12-27 22:38:39 +0900

    feat: 게시글 수정/조회 api 구현

--- src/main/java/com/eoc940/book/springboot/web/PostsApiController.java
+++ src/main/java/com/eoc940/book/springboot/web/PostsApiController.java
@@ -1,11 +1,12 @@
 package com.eoc940.book.springboot.web;
 
+import com.eoc940.book.springboot.domain.posts.Posts;
 import com.eoc940.book.springboot.service.posts.PostsService;
+import com.eoc940.book.springboot.web.dto.PostsResponseDto;
 import com.eoc940.book.springboot.web.dto.PostsSaveRequestDto;
+import com.eoc940.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +17,15 @@ public class PostsApiController {
     @PostMapping("/api/va/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
--- src/test/java/com/eoc940/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/eoc940/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,7 +1,5 @@
 package com.eoc940.book.springboot.domain.posts;
 
-import com.eoc940.book.springboot.domain.posts.Posts;
-import com.eoc940.book.springboot.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
