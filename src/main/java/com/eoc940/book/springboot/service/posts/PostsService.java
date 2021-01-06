4edda92cf9259fbb09900ea8a4c9ab68a5d9de2a
eoc940 <hgk0000@naver.com>
2021-01-06 22:37:54 +0900

    feat: 게시글 전체 조회 화면 구현

--- src/main/java/com/eoc940/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/eoc940/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.eoc940.book.springboot.service.posts;
 
 import com.eoc940.book.springboot.domain.posts.Posts;
 import com.eoc940.book.springboot.domain.posts.PostsRepository;
+import com.eoc940.book.springboot.web.dto.PostsListResponseDto;
 import com.eoc940.book.springboot.web.dto.PostsResponseDto;
 import com.eoc940.book.springboot.web.dto.PostsSaveRequestDto;
 import com.eoc940.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +37,13 @@ public class PostsService {
                 IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
